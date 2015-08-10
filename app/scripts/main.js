a133d5ca556201e94181b5ba906be00eaede7215
Robin Fox <robinfoxin@gmail.com>
2015-08-10 13:05:16 -0700

    Working on HTML, CSS, JS

---
+++ app/scripts/main.js
@@ -0,0 +1,45 @@
+// start of document
+$(document).ready(function() {
+
+  // object for each type of rap with arrays for rap type chosen
+  var south = {
+    nineties: [
+      'Freak Nasty, "Da Dip"(1996)',
+      'DJ Uncle Al, "Uncle Al Mix It Up"(1993)',
+      'Geto Boys, "Do it Like a G.O."(1990)',
+      'Juvenile, "Ha!"(1998)',
+      'Underground Kingz(UGK), "Front, Back & Side-to-Side" (1994)',
+      'Projec Pat, "You Know the Biss"(1999)',
+      'Three 6 Mafia, "Hit a Muthafucka"(1997)',
+      'Diamond feat. D-Roc, "Bankhead Bounce"(1996)',
+      'MC T. Tucker & DJ Irv, "Where Dey At"(1991)',
+      'Mia X, "Da Payback"(1993)'
+    ],
+    millennium: [
+      'Kingpin Skinny Pimp, "Where Ya From?"(2000)',
+      'Project Pat feat. La\' Chat, "Chickenhead"(2001)',
+      'D4L, "Laffy Taffy"(2005)',
+      'Trap Squad, "What\'s Happenin?"(2006)',
+      'Trick Daddy, "In da Wind"(2002)',
+      'Eightball & MJG, "Boom Boom"(2001)',
+      'DJ Michael 5000 watts, "Weight a Minute Freestyle"(2004)',
+      'S.W.A., "We #1"(2002)',
+      'Missy Elliot, "Get Ur Freak On"(2001)',
+      'Clipse feat. Pharrell Williams, "Mr. Me Too"(2006)'
+    ],
+    present: [
+      'Future feat. Drake, "Where Ya At"(2015)',
+      'PJ feat. Hit Boy, "I Mean It(2015)"',
+      'Rae Sremmurd, "My X"(2015)',
+      'Brodinski feat. Bloody Jay, "Us"(2015)',
+      'Rich Homie Quan, "Flex (Ooh, Ooh, Ooh)"(2015)',
+      'Ceraadi, "Favorite Song"(2015)',
+      'Johnny Cinco, "Oh Yeah"(2015)',
+      'Ca$h Out "You Should Be Ready"(2015)',
+      'Bandit Gang Marco feat. Bandit Gang, "No Cash"(2015)',
+      'Bobby Brackins feat. Zendaya and Jeremih, "My Jam"(2015)'
+    ]
+  };
+
+
+});
